c5b90524e3dbd205880a64dcdaf94c7e4787cbc9
David Moeller <davidmoeller@Davids-MacBook-Pro.local>
2017-10-21 15:09:18 -0400

    basic functionality

---
+++ backEnd/handleRequests.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import os
+import json
+import socket
+import requests
+import datetime
+
+import pdb
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+URL = 'http://192.241.193.9:3000'
+
+app = Flask(__name__)
+
+@app.route('/getInfo', methods=['POST'])
+def main():
+
+    message = None
+
+    if request.form['data'] == 'test':
+        message = 'Got it!'
+
+    resp = app.make_response(message)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+if (__name__ == "__main__"):
+    app.run(host='192.241.193.9', port=3001)

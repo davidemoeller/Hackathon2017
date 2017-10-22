216733fc038677d8faf7e3278c733596722972fb
Tommy Zarick <tzarick7@gmail.com>
2017-10-22 04:26:36 -0400

    createEvent finished hopefully

--- frontEnd/sendEventToServer.js
+++ frontEnd/sendEventToServer.js
@@ -21,27 +21,27 @@ $(function sendEvent(geoFence){
         var description = 'Lit smack party';
         var invite = "['Mason Hayes', 'Tommy Zarick', 'David Moeller', 'Yomali Kader', 'Bill Cosby']";
         alert(coords);
-        // $.ajax({
-        //     url: 'http://192.241.193.9:3001/createEvent',
-        //     data: {
-        //       'loc': coords,
-        //       'date': date,
-        //       'time': time,
-        //       'description': description,
-        //       'invite': invite
-        //     },
-        //     type: 'POST',
-        //     success: function(err, response, body) {
-        //         // alert("HELLO");
-        //         // var  = JSON.parse(body);
-        //         // console.log(err);
-        //         //
-        //         // alert(JSON.stringify(body.responseJSON.list));
-        //         // alert(response);
-        //
-        //     },
-        //     error: function(error) {
-        //         console.log(error);
-        //     }
-        // });
+        $.ajax({
+            url: 'http://192.241.193.9:3001/createEvent',
+            data: {
+              'loc': coords,
+              'date': date,
+              'time': time,
+              'description': description,
+              'invite': invite
+            },
+            type: 'POST',
+            success: function(err, response, body) {
+                // alert("HELLO");
+                // var  = JSON.parse(body);
+                // console.log(err);
+                //
+                // alert(JSON.stringify(body.responseJSON.list));
+                // alert(response);
+
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
 });

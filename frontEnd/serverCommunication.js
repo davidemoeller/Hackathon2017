ac33d2dbba831640008c9a6ca4491c4f1b97ebb0
Tommy Zarick <tzarick7@gmail.com>
2017-10-21 21:19:38 -0400

    front end changes, map kind of working

--- frontEnd/serverCommunication.js
+++ frontEnd/serverCommunication.js
@@ -1,17 +1,28 @@
-const net = require("net");
-
-// Create a socket (client) that connects to the server
-var socket = new net.Socket();
-socket.connect(3000, "192.241.193.9", function () {
-    console.log("Client: Connected to server");
-});
-
-// Let's handle the data we get from the server
-socket.on("data", function (data) {
-    data = JSON.parse(data);
-    console.log("Response from server: %s", data.response);
-    // Respond back
-    socket.write(JSON.stringify({ response: "Hey there server!" }));
-    // Close the connection
-    socket.end();
+$(function() {
+    $('button').click(function() {
+//        var user = $('#txtUsername').val();
+//        var pass = $('#txtPassword').val();
+        var coords = '75.77 85.32 -21.22 102.43';
+        var date = '10/21/2017';
+        var time = '20:00';
+        var description = 'Lit smack party';
+        var invite = "['Mason Hayes', 'Tommy Zarick', 'David Moeller', 'Yomali Kader', 'Bill Cosby']";
+        $.ajax({
+            url: 'http://192.241.193.9:3001/createEvent',
+            data: {
+              'loc': coords,
+              'date': date,
+              'time': time,
+              'description': description,
+              'invite': invite
+            },
+            type: 'POST',
+            success: function(response) {
+                alert(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
 });

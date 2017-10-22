eb8640337c8d94654a0d0e0ba038698e96bc69ce
Tommy Zarick <tzarick7@gmail.com>
2017-10-22 03:45:56 -0400

    submit button

---
+++ frontEnd/sendEventToServer.js
@@ -0,0 +1,47 @@
+// TRASH
+$(function sendEvent(geoFence){
+        alert("HELLO");
+        var vertices = geoFence.getPath();
+        var coords = [];
+
+        for (var i = 0; i < vertices.getLength(); i++) {
+          var xy = vertices.getAt(i)
+          if (i == 0) {
+            coords.push(xy.lat()); // North side of rectangle
+            coords.push(xy.lng()); // West side of rectangle
+          }
+          else if (i == 2) {
+            coords.push(xy.lat()); // South side of rectangle
+            coords.push(xy.lng());
+          }
+        }
+        var coords = '\"' + coords + '\"';
+        var date = '10/21/2017 16:30';
+        var time = '20:00' // TODO: delete
+        var description = 'Lit smack party';
+        var invite = "['Mason Hayes', 'Tommy Zarick', 'David Moeller', 'Yomali Kader', 'Bill Cosby']";
+        alert(coords);
+        // $.ajax({
+        //     url: 'http://192.241.193.9:3001/createEvent',
+        //     data: {
+        //       'loc': coords,
+        //       'date': date,
+        //       'time': time,
+        //       'description': description,
+        //       'invite': invite
+        //     },
+        //     type: 'POST',
+        //     success: function(err, response, body) {
+        //         // alert("HELLO");
+        //         // var  = JSON.parse(body);
+        //         // console.log(err);
+        //         //
+        //         // alert(JSON.stringify(body.responseJSON.list));
+        //         // alert(response);
+        //
+        //     },
+        //     error: function(error) {
+        //         console.log(error);
+        //     }
+        // });
+});
--- frontEnd/serverCommunication.js
+++ frontEnd/serverCommunication.js
@@ -1,24 +1,36 @@
-$(function() {
+$(function {
     $('button').click(function() {
 //        var user = $('#txtUsername').val();
 //        var pass = $('#txtPassword').val();
         var coords = '75.77 85.32 -21.22 102.43';
-        var date = '10/21/2017';
-        var time = '20:00';
-        var description = 'Lit smack party';
-        var invite = "['Mason Hayes', 'Tommy Zarick', 'David Moeller', 'Yomali Kader', 'Bill Cosby']";
+        var date-time = document.getElementById("date-time");
+        var time = '20:00'
+        var description = document.getElementById("document");
+        var invite = "[" + document.getElementById("inviteList") + "]";
+
+
         $.ajax({
-            url: 'http://192.241.193.9:3001/createEvent',
+            url: 'http://192.241.193.9:3001/login',
             data: {
-              'loc': coords,
-              'date': date,
-              'time': time,
-              'description': description,
-              'invite': invite
+              // 'loc': coords,
+              // 'date': date,
+              // 'time': time,
+              // 'description': description,
+              // 'invite': invite
+              'name': "Tommy Zarick"
             },
             type: 'POST',
-            success: function(response) {
+            success: function(err, response, body) {
+                // alert("HELLO");
+                // var  = JSON.parse(body);
+                console.log(err);
+
+                alert(JSON.stringify(body.responseJSON.list));
                 alert(response);
+                // alert(JSON.parse(response));
+                // var list = body.list;
+                // alert("HELLP");
+                // alert(list);
             },
             error: function(error) {
                 console.log(error);
